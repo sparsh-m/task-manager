f512daf6e38356f120d57791c1f52cd25f7af33e
sparsh-m <sparshmisra1@gmail.com>
2020-12-30 01:11:44 +0530

    First Commit

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+//CRUD
+const {MongoClient, ObjectID} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client)=>{
+    if(error){
+        return console.log('Unable to connect to database')
+    }
+    const db = client.db(databaseName)
+
+    db.collection('users').deleteOne({
+        age:62
+    }).then((result)=>{
+        console.log(result.deletedCount)
+    }).catch((error)=>{
+        console.log(error)
+    })
+
+
+})
+
